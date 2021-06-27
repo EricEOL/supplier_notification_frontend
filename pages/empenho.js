e07764b06c1bcda2eee929edecae3c6e671eba54
EricEOL <lopes.eric051@gmail.com>
2021-06-27 17:39:03 -0300

    Loop de requisições a api para verificação de notificação dos fornecedores

--- pages/empenho.js
+++ pages/empenho.js
@@ -1,34 +1,55 @@
-import { useState } from "react"
-import axios from 'axios';
+import { useEffect, useState } from "react"
+import { api } from '../config/api';
+
 
 export default function Empenho() {
-  
+
   const [empenho, setEmpenho] = useState();
   const [email, setEmail] = useState();
   const [date, setDate] = useState();
 
+  useEffect(() => {
+    notificationSuppliers();
+  }, []);
 
-  async function handleSubmit(event)  {
+  setInterval(() => {
+    notificationSuppliers();
+  }, 50000);
+
+  async function notificationSuppliers() {
+    await api.get('empenhos/notification');
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    await axios.post('http://localhost:3333/empenhos', {
+    await api.post('empenhos', {
       empenho,
       email,
       date
     })
   }
-  
+
   return (
     <main>
-
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Empenho" onChange={(e) => setEmpenho(e.target.value)}/>
-        <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-        <input type="date" onChange={(e) => setDate(e.target.value)}/>
+        <div>
+          <strong>Nota de Empenho</strong>
+          <input type="text" placeholder="Ex: 2021NE000111" onChange={(e) => setEmpenho(e.target.value)} />
+        </div>
+
+        <div>
+          <strong>Email do fornecedor</strong>
+          <input type="text" placeholder="Ex: pandasuprir@gmail.com" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+
+        <div>
+          <strong>Data de envio da NE ao fornecedor</strong>
+          <input type="date" onChange={(e) => setDate(e.target.value)} />
+        </div>
 
         <button type="submit">Inserir</button>
       </form>
-
     </main>
   )
 }
